a0e2057412f895d76d7e4f1320b4e95932abecfc
alexe <alexeiastrakh@gmail.com>
2022-08-31 13:32:15 +0300

    Initial

---
+++ src/main/java/org/example/ThreadRace.java
@@ -0,0 +1,40 @@
+package org.example;
+
+class Counter implements Runnable
+{
+    private int c = 0;
+    public void increment()
+    {
+        try
+        {
+            Thread.sleep(10);
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+        c++;
+    }
+    public void decrement()
+    {
+        c--;
+    }
+    public int getValue()
+    {
+        return c;
+    }
+    @Override
+    public void run()
+    {
+        //synchronized(this)
+        {
+
+            this.increment();
+            System.out.println("Value after increment " + Thread.currentThread().getName() + " " + this.getValue());
+
+            this.decrement();
+            System.out.println("Value after decrement " + Thread.currentThread().getName() + " " + this.getValue());
+        }
+    }
+}
+
